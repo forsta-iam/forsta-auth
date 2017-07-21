d5389bf6f9360dab926d2aa0f488aeb48a399058
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-07-21 09:08:59 +0100

    Remove test moved to idm_broker

--- idm_auth/tests/utils.py
+++ idm_auth/tests/utils.py
@@ -9,21 +9,6 @@ import functools
 import kombu
 from django.apps import apps
 
-from idm_broker.consumer import BrokerTaskConsumer
-
-
-class BrokerTaskConsumerTestCaseMixin(object):
-    def setUp(self):
-        self.broker_task_consumer = BrokerTaskConsumer()
-        self.broker_task_consumer_thread = threading.Thread(target=self.broker_task_consumer)
-        self.broker_task_consumer_thread.start()
-        super().setUp()
-
-    def tearDown(self):
-        self.broker_task_consumer.should_stop = True
-        self.broker_task_consumer_thread.join()
-        super().tearDown()
-
 
 def creates_idm_core_user(f):
     """Function decorator that mocks the idm-core API that creates a user"""
