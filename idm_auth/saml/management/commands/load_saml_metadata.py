c6b216e8182f83beb3bedc781087bd1ac0a9db8b
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-08-04 21:46:48 +0100

    Add metadata signature verification for SAML

--- idm_auth/saml/management/commands/load_saml_metadata.py
+++ idm_auth/saml/management/commands/load_saml_metadata.py
@@ -1,17 +1,32 @@
+import argparse
 import sys
 from django.core.management import BaseCommand
 from lxml import etree
+import xmlsec
 
 from idm_auth.saml.models import IDP
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        assert isinstance(parser, argparse.ArgumentParser)
+        parser.add_argument('certificate',
+                            help='Path to the certificate used to sign the metadata file, for verification')
+
     def handle(self, **opts):
         metadata = etree.parse(sys.stdin)
         NS = {'namespaces': {'saml': 'urn:oasis:names:tc:SAML:2.0:metadata',
                              'ds': 'http://www.w3.org/2000/09/xmldsig#',
                              'mdattr': 'urn:oasis:names:tc:SAML:metadata:attribute',
                              'assertion': 'urn:oasis:names:tc:SAML:2.0:assertion'}}
+
+        # Verify the signature
+        ctx = xmlsec.SignatureContext()
+        ctx.key = xmlsec.Key.from_file(opts['certificate'], xmlsec.constants.KeyDataFormatCertPem)
+        xmlsec.tree.add_ids(metadata.getroot(), ["ID"])
+        signature_node = xmlsec.tree.find_node(metadata.getroot(), xmlsec.constants.NodeSignature)
+        ctx.verify(signature_node)
+
         idp_descriptors = metadata.xpath('''
             //saml:EntityDescriptor[
                 not(
@@ -26,7 +41,6 @@ class Command(BaseCommand):
                         @Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"]]]''', **NS)
         seen_idp_names = set()
         for idp_descriptor in idp_descriptors:
-            print(idp_descriptor.attrib)
             try:
                 idp = IDP.objects.get(name=idp_descriptor.attrib['ID'])
             except IDP.DoesNotExist:
