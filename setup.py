4078f289db29dcc26aa86e161f6be851c1abb5d6
Alexander Dutton <adutton@oni.bio>
2019-05-21 15:50:43 +0100

    Make the project into a library

---
+++ setup.py
@@ -0,0 +1,49 @@
+import os.path
+import re
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+with open(os.path.join('idm_auth', '__init__.py')) as f:
+    version = re.search(
+        r'''^__version__\s*=\s*(['"])(.*)\1''', f.read(), re.MULTILINE
+    ).group(2)
+
+setuptools.setup(
+    name='forsta-auth',
+    version=version,
+    author='Alexander Dutton',
+    author_email='forsta@alexdutton.co.uk',
+    description='A federated authentication service',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/forsta-iam/forsta-auth",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django :: 2.2",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    install_requires=[
+        "celery",
+        "Django",
+        "djangorestframework",
+        "django-reversion",
+        "django-zxcvbn-password",
+        "requests",
+        "social-auth-app-django",
+        "django-otp",
+        "django-two-factor-auth",
+        "django-registration",
+        "django-oidc-provider",
+        "django-dirtyfields",
+    ],
+    extras_require={
+        "kerberos": ["pykerberos", "requests-negotiate"],
+        "saml": ["xmlsec", "python3-saml"],
+    },
+)
