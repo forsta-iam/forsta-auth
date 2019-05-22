9c966fa003e642e85f37fdaf2e74760ab6c656be
Alexander Dutton <adutton@oni.bio>
2019-05-22 13:43:01 +0100

    Add a wsgi module for the example app

---
+++ examples/simple/forsta_auth_simple/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for forsta_auth_simple project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forsta_auth_simple.settings')
+
+application = get_wsgi_application()
+
