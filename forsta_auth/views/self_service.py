3a3d4889f6d83f6c02b9c33fcf4cf0175ee7ca6a
Alexander Dutton <adutton@oni.bio>
2019-06-05 11:49:22 +0100

    SocialLoginsView now selects social backends for display properly.

--- forsta_auth/views/self_service.py
+++ forsta_auth/views/self_service.py
@@ -36,7 +36,7 @@ class SocialLoginsView(LoginRequiredMixin, TemplateView):
         return {
             'associated': [BackendMeta.wrap(user_social_auth)
                            for user_social_auth in self.request.user.social_auth.all()],
-            'social_backends': list(sorted([bm for bm in backend_meta.BackendMeta.registry.values() if bm.backend_id != 'saml'], key=lambda sb: sb.name)),
+            'social_backends': list(sorted([bm for bm in backend_meta.BackendMeta.registry.values() if bm.show], key=lambda sb: sb.name)),
         }
 
 
