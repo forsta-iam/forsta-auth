0a84d9f096aad426ab2f08949c3fa632f424f66d
Alexander Dutton <adutton@oni.bio>
2019-06-06 12:28:14 +0100

    Remove contrib.sites from INSTALLED_APPS
    This means that Django uses RequestSite instead of the Site model for the
    domain, which means less configuration for simple cases (as we don't need a
    Site fixture).

--- forsta_auth/settings.py
+++ forsta_auth/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.staticfiles',
     'social_django',
     'reversion',
