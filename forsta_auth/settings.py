5cb59728e2f95feb68cdac57174f5609622d0911
Alexander Dutton <adutton@oni.bio>
2019-06-07 10:30:38 +0100

    Improve signup/activation templates

--- forsta_auth/settings.py
+++ forsta_auth/settings.py
@@ -221,6 +221,7 @@ for key in os.environ:
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = True
 
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
