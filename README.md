c73a5797d8d5a68e266671543e3877f5fc568b20
Alexander Dutton <adutton@oni.bio>
2019-09-16 13:12:47 +0100

    Add script to document env vars in README.md

--- README.md
+++ README.md
@@ -2,3 +2,39 @@
 
 [![Build Status](https://travis-ci.org/forsta-iam/forsta-auth.svg?branch=master)](https://travis-ci.org/forsta-iam/forsta-auth) [![codecov](https://codecov.io/gh/forsta-iam/forsta-auth/branch/master/graph/badge.svg)](https://codecov.io/gh/forsta-iam/forsta-auth)
 
+
+## Environment variables
+
+Name | Cast | Default | Description
+---- | ---- | ------- | -----------
+DEBUG | `bool` | `False` |
+TEMPLATE_DEBUG | `bool` | `DEBUG` |
+**SECRET_KEY** |  |  |
+USE_TZ | `bool` | `True` |
+TIME_ZONE |  | `'Europe/London'` |
+ALLOWED_HOSTS | `list` | `['*'] if DEBUG else []` |
+DATABASE_URL |  | `'postgres:///forsta-auth'` |
+**STATIC_ROOT** |  |  |
+**MEDIA_ROOT** |  |  |
+BROKER_TRANSPORT |  | `'amqp'` |
+BROKER_HOSTNAME |  | `'localhost'` |
+BROKER_SSL | `bool` | `True` |
+BROKER_VHOST |  | `'/'` |
+BROKER_USERNAME |  | `'guest'` |
+BROKER_PASSWORD |  | `'guest'` |
+BROKER_PREFIX |  | `'idm.auth.'` |
+**CELERY_BROKER_URL** |  |  |
+IDM_CORE_URL |  | `'http://localhost:8000/'` |
+IDM_CORE_API_URL |  | `'http://localhost:8000/api/'` |
+EMAIL_HOST |  | `None` |
+EMAIL_HOST_USER |  | `None` |
+EMAIL_HOST_PASSWORD |  | `None` |
+EMAIL_PORT | `int` | `587` |
+EMAIL_USE_TLS | `bool` | `True` |
+EMAIL_BACKEND |  | `'django.core.mail.backends.console.EmailBackend'` |
+DEFAULT_REALM |  | `'EXAMPLE.COM'` |
+KADMIN_PRINCIPAL_NAME |  | `None` |
+CLIENT_PRINCIPAL_NAME |  | `None` |
+KERBEROS_ENABLED | `bool` | `None` |
+SSH_KEYS_ENABLED | `bool` | `None` |
+SAML_ENABLED | `bool` | `None` |
---
+++ bin/sync_env_var_docs.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import ast
+import os
+import pathlib
+import re
+
+import astor
+
+settings_path = pathlib.Path(os.path.dirname(__file__)) / '..' / 'forsta_auth' / 'settings.py'
+settings = settings_path.read_text()
+
+readme_path = pathlib.Path(os.path.dirname(__file__)) / '..' / 'README.md'
+
+vars = []
+
+for match in re.finditer(r'\W(?P<env>env(?P<db>\.db)?\(.*\)),?(\s*#\s+(?P<comment>.*))?$', settings, re.MULTILINE):
+    var = match.groupdict()
+
+    call = ast.parse(match['env']).body[0].value
+    args, kwargs = call.args, call.keywords
+
+    if var['db']:
+        var['name'] = 'DATABASE_URL'
+    else:
+        var['name'] = args[0].s
+
+    for kwarg in kwargs:
+        value = astor.to_source(kwarg.value).strip()
+        if value.startswith('(') and value.endswith(')'):
+            value = value[1:-1]
+        if value.startswith('"""') and value.endswith('"""'):
+            value = value[2:-2]
+        if value.startswith('"') and value.endswith('"') and "'" not in value:
+            value = "'" + value[1:-1] + "'"
+        if kwarg.arg in ('default', 'cast'):
+            var[kwarg.arg] = '`' + value.replace('|', '\\|').replace('`', '\\`') + '`'
+
+    vars.append(var)
+
+new_readme = []
+
+with readme_path.open() as readme:
+    for line in readme:
+        new_readme.append(line)
+        if line == '## Environment variables\n':
+            break
+
+    new_readme.extend([
+        "\n",
+        "Name | Cast | Default | Description\n",
+        "---- | ---- | ------- | -----------\n",
+    ])
+
+    for var in vars:
+        name = var['name'].replace('|', '\|')
+        if not var.get('default'):
+            name = f'**{name}**'
+        new_readme.append("{} | {} | {} | {}".format(
+            name,
+            var.get('cast') or '',
+            var.get('default') or '',
+            (var.get('comment') or '').replace('|', '\|'),
+        ).strip() + '\n')
+
+    resume = False
+    for line in readme:
+        if line.startswith('## '):
+            resume = True
+        if resume:
+            new_readme.append(line)
+
+readme_path.write_text(''.join(new_readme))
